da61a534ca5cb446dbdb7c14b527040abaf52b14
Jerald-Joyson <121479325+Jerald-Joyson@users.noreply.github.com>
2023-03-17 16:32:17 +0530

    updated the High score

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
-var counter=0;
+var counter = 0;
 var gameStarted = false;
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+
+let highScore = localStorage.getItem("high-scores") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
 
 function startGame() {
     if (gameStarted) {
@@ -9,23 +14,23 @@ function startGame() {
     }
     gameStarted = true;
     block.style.animation = "block 1s infinite linear";
-    setInterval(function() {
+    setInterval(function () {
         let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-        if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
             block.style.animation = "none";
-            if(confirm("Game Over. score: "+Math.floor(counter/100)+"\nDo you want to play again?")){
+            if (confirm("Game Over. score: " + Math.floor(counter / 100) + "\nDo you want to play again?")) {
                 window.location.reload();
             }
-            else{
+            else {
                 openPage("thankyou.html");
-                // counter=0;
-                // document.getElementById("scoreSpan").innerHTML = 0;
             }
-            // block.style.animation = "block 1s infinite linear";
-        }else{
+        } else {
             counter++;
-            document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+            document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+            highScore = Math.floor(counter / 100) >= highScore ? Math.floor(counter / 100) : highScore; // if score > high score => high score = score
+            localStorage.setItem("high-scores", highScore);
+            highScoreElement.innerText = `High Score: ${highScore}`;
         }
     }, 10);
 }
@@ -34,10 +39,10 @@ function openPage(pageUrl) {
     window.location.href = pageUrl;
 }
 
-function jump(){
-    if(character.classList == "animate"){return}
+function jump() {
+    if (character.classList == "animate") { return }
     character.classList.add("animate");
-    setTimeout(function(){
+    setTimeout(function () {
         character.classList.remove("animate");
-    },300);
+    }, 300);
 }
